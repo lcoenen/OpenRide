e45b6029f984731a6929cae4d145b4c3ab68129a
Loïc Coenen <ijwtcymb@gmail.com>
2018-02-18 18:35:30 +0100

    Ride board skeletton

--- src/pages/ride-board/ride-board.ts
+++ src/pages/ride-board/ride-board.ts
@@ -15,9 +15,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RideBoardPage {
 
+  boardpage: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     this.boardpage = 'riders';
+
 
   }
 
