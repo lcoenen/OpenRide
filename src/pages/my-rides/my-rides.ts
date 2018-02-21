58f16f6b10757238126bb93f80f6aadef0ce985d
Loïc Coenen <ijwtcymb@gmail.com>
2018-02-21 11:58:41 +0100

    Added my rides page and link to ride_board

--- src/pages/my-rides/my-rides.ts
+++ src/pages/my-rides/my-rides.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Rider } from './rider';
+import { RidersMockData } from './rider-mock';
+
+import { RideBoardPage } from '../ride-board/ride-board'; 
+
 /**
  * Generated class for the MyRidesPage page.
  *
@@ -15,11 +20,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyRidesPage {
 
+  riders: Rider[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.riders = RidersMockData;
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyRidesPage');
+  }
+
+  open_board(rider){
+  
+    console.log("Ouverture du rider", rider); 
+    this.navCtrl.push(RideBoardPage);
+  
   }
 
 }
