5937fc9b3b22a4cc480b4b4d867e95b511e1271e
Loïc Coenen <ijwtcymb@gmail.com>
2018-03-06 20:45:34 +0100

    Added some tests

--- server/test/matching.ts
+++ server/test/matching.ts
@@ -1,12 +1,37 @@
-import { expect } from 'chai';
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+/* 
+ * Both librairies have limited support of ES6 import.
+ * See https://github.com/DefinitelyTyped/DefinitelyTyped/issues/19480
+*/
+
+chai.use(chaiHttp);
+
+import { expect } from 'chai' ;
 import 'mocha';
 
-describe('First test',  () => {
+const url: string = 'localhost:3000';
 
-	it('should makes you scream HEEEHOOO', () => {
+describe('rides',  () => {
+	it("should get a list of rides");
+	it("should get a specific ride");
+	it("should add and remove riders inside ride");
+	it("should delete ride when needed");
 
-		expect('HEHO').to.equal('HEHO');
+	it("should post a new ride", () => {
+
+		chai.request(url)
+			.post('/api/rides')
+			.send({
+
+					
+
+			});
 
 	});
+	it("should post a new ride request", () => {
 
+
+	});
 });
