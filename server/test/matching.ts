e1abc4d4b34c6f089dae2cbb7f6f7f41b7e5a3ba
Loïc Coenen <ijwtcymb@gmail.com>
2018-03-06 17:57:58 +0100

    Added first test

---
+++ server/test/matching.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('First test',  () => {
+
+	it('should makes you scream HEEEHOOO', () => {
+
+		expect('HEHO').to.equal('HEHO');
+
+	});
+
+});
