d8d62c0189c01c26cbae93d61b36e61bb5206a8f
Loïc Coenen <ijwtcymb@gmail.com>
2018-03-21 14:22:47 +0100

    Added the resolving in stream processing
    (Broken, no session file added)

--- client/src/providers/riders/riders.ts
+++ client/src/providers/riders/riders.ts
@@ -30,32 +30,22 @@ export class RidersProvider {
 	 */
 	invitable_riders(): Observable<Ride[]> {
 
-		console.log('Trying the API call');
-
-		console.log(`Trying to reach ${ settings.apiEndpoint }/api/rides/${ this.currentRideId }/matches`)
-
+		// A stream that emit one Link[] 
 		return this.httpClient.get(`${ settings.apiEndpoint }/api/rides/${ this.currentRideId }/matches`)
 			.mergeMap((links: Link[]) => {
-
+				// Return a stream that emit each Link 
 				return Observable.from(links) 
 					.mergeMap((link: Link) : Observable<Ride> => {
-
+						// Return a stream that emit each resolved Ride
 						return this.httpClient.get(`${ settings.apiEndpoint }${ link['@id'] }`);
-
 					})
-					.mergeMap((ride: Ride) => {
-	
-						console.log(`ligne 48`)
-						console.log(ride)
+					.mergeMap((ride: Ride): Observable<Ride> => {
 						if(ride.driver) {
-							console.log(`${ settings.apiEndpoint }${ ride.driver['@id'] }`)
 							return this.httpClient
 								.get(`${ settings.apiEndpoint }${ ride.driver['@id'] }`)
 								.map( (user: User) => {
-									console.log(`l.54`)
-									console.log(user)
-								  return ride.driver = user;  
-
+								  ride.driver = user;  
+									return ride;
 								})
 							}
 						else return Observable.of(ride);
@@ -63,13 +53,6 @@ export class RidersProvider {
 					})
 			})
 			.toArray()
-			.subscribe((data) => {
-
-				console.log(`line 50`)
-
-				console.log(data)  
-
-			})
 
 		// 		return this.httpClient.get(`${ settings.apiEndpoint }/api/rides/${ this.currentRideId }/matches`)
 		// 			.mergeMap((links: Link[]) => {
